35b137d61588ba375742324caf33142fc0347253
unknown <niyaz.hashmi@asians.cloud>
2022-10-27 20:21:17 +0530

    utility methods and page classes created

--- src/main/java/com/gaius/console/pages/AddDomainPage.java
+++ src/main/java/com/gaius/console/pages/AddDomainPage.java
@@ -1,13 +1,39 @@
 package com.gaius.console.pages;
 
+import com.gaius.console.utils.ElementUtils;
 import com.microsoft.playwright.Page;
 
 public class AddDomainPage {
 	
 	private Page page;
+	private ElementUtils elementUtils;
+	
 	public AddDomainPage(Page page) {
 		this.page = page;
+		elementUtils = new ElementUtils(page);
 	}	
+	
+	private String domainNameTxtBox="#domain_name";
+	private String addNewTargetTxtBox="#add_new_target";
+	private String addNewTargetAddBtn= "div[class='d-flex justify-content-end mt-n4'] button[type='button']";
+	private String upStreamProtocol ="div[class='ant-btn-group'] button:nth-child(1)";
+	private String shadowDomainTab = "text=Shadow Domain";
+	private String shadowDomainTxtBox = "#uid-n03l1pmaevg";
+	private String addPlusIcon ="button[class='btn close btn-black btn-sm']";
+	private String saveBtn = "div[class='d-flex justify-content-end mt-n4'] button[type='button']";
+	
+	public void doFillBasicConfiguration(String domainNameVal, String addNewTargetVal,String shadowDomainVal) {
+		elementUtils.doFill(domainNameTxtBox, domainNameVal);
+		elementUtils.scrollDown(shadowDomainVal);
+		elementUtils.doFill(addNewTargetTxtBox, addNewTargetVal);
+		elementUtils.doClick(addNewTargetAddBtn);
+		elementUtils.doClick(upStreamProtocol);
+		elementUtils.doClickLastLocatorElement(shadowDomainTab);
+		elementUtils.doFill(shadowDomainTxtBox, shadowDomainVal);
+		elementUtils.doClick(addPlusIcon);
+		elementUtils.doClick(saveBtn);
+	}
+	
 	
 	
 	
--- src/main/java/com/gaius/console/utils/ElementUtils.java
+++ src/main/java/com/gaius/console/utils/ElementUtils.java
@@ -1,6 +1,12 @@
 package com.gaius.console.utils;
 
-import com.microsoft.playwright.Locator.FillOptions;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.microsoft.playwright.Keyboard;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 public class ElementUtils {
@@ -9,6 +15,7 @@ public class ElementUtils {
 	public ElementUtils(Page page) {
 		this.page = page;
 	}
+	
 	
 	public void doFill(String locator,String value) {
 		page.fill(locator, value);
@@ -20,5 +27,76 @@ public class ElementUtils {
 	public boolean isVisible(String locator) {
 		return page.isVisible(locator);
 	}
-
+	
+	public String getLocatorElementText(String locator) {
+		String text = page.locator(locator).textContent();
+		return text;
+	}
+	
+	public List<String> getLocatorElementsText(String locator) {
+		List<String> text = page.locator(locator).allTextContents();
+		List<String> elementList = new ArrayList<String>();
+		for(String allElementTexts: text) {
+			elementList.add(allElementTexts);
+			return elementList;
+		}
+		return null;
+	}
+	
+	public void doClickFirstLocatorElement(String locator) {
+		 page.locator(locator).first().click();
+	}
+	
+	public void doClickLastLocatorElement(String locator) {
+		 page.locator(locator).last().click();
+	}
+	
+	public void doClickLocatorElementsText(String locator, String elementValue) {
+		
+		Locator text = page.locator(locator);
+		for(int i=0;i<text.count();i++) {
+			
+		String element= text.nth(i).textContent();
+			if(element.equals(elementValue)) {
+				text.nth(i).click();
+				break;
+			}
+		}
+		return;
+	}
+	
+	public String getLocatorElementTextInFrame(String frameLocator, String elementLocator){
+	
+			String elementValue= page.frame(frameLocator).locator(elementLocator).textContent();
+			return elementValue;
+	}
+	
+	public void doClickLocatorElementTextInFrame(String frameLocator, String elementLocator){
+		
+		page.frame(frameLocator).locator(elementLocator).click();
+	}
+	
+	public void doFillLocatorElementTextInFrame(String frameLocator, String elementLocator,String text){
+		
+		page.frame(frameLocator).locator(elementLocator).fill(text);
+	}
+	public List<String> getTableData(String scopeLocator) 
+	{
+		List<String> row = page.locator(scopeLocator).allInnerTexts();
+		List<String> dataList = new ArrayList<>();
+		for(String element:row) {
+			 dataList.add(element);
+			 return dataList;
+		}
+		return null;
+	}
+	
+	public void getElementFromTable(String tableLocator,String scopeLocator, String elementToBeSearch, String locator) {
+		Locator row = page.locator(tableLocator);
+		row.locator(scopeLocator, new Locator.LocatorOptions().setHasText(elementToBeSearch)).locator(locator).click();
+	}
+	
+	public void scrollDown(String locator) {
+		page.keyboard().press("PageDown");
+	}
 }
